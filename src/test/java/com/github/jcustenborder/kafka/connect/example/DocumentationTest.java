151f54b7d60bed42867b751698203a3ba5d5c8fa
Jeremy Custenborder <jcustenborder@gmail.com>
2020-04-02 09:12:46 -0500

    Updated (#2)

---
+++ src/test/java/com/github/jcustenborder/kafka/connect/example/DocumentationTest.java
@@ -0,0 +1,7 @@
+package com.github.jcustenborder.kafka.connect.example;
+
+import com.github.jcustenborder.kafka.connect.utils.BaseDocumentationTest;
+
+public class DocumentationTest extends BaseDocumentationTest {
+
+}
